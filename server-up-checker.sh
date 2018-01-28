e371d5440cc5c371552ff6479f2e7f427f273e93
jorgeluizms <jorge.silva93@gmail.com>
2018-01-27 23:41:09 -0200

    Added a script that checks every registered server and save the results into a mysql database

---
+++ server-up-checker.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#Build list of servers to be read
+echo "SELECT IP, id from servers ;" | mysql -u dummy Polimet| sed -n '1!p'>servers.txt
+#Automatically computes ping for each server in servers.txt
+while IFS='' read -r line || [[ -n "$line" ]]; do
+line_data=( $line )
+ping -c 1 ${line_data[0]} &>/dev/null
+if (($?==0)); then
+server_status="'online'"
+else
+server_status="'offline'"
+fi
+echo ${line_data[0]}": " $server_status
+mysql -u dummy -D Polimet -e "INSERT INTO uptimes VALUES (${line_data[1]},$server_status,NULL)";
+done < "servers.txt"

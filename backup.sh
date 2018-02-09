ba30384703a1b2c5b30df6e4d86ac8d1ee510716
jorgeluizms <jorge.silva93@gmail.com>
2018-02-09 21:42:26 -0200

    Added indentation to backup.sh. Added logging to backup.sh

--- backup.sh
+++ backup.sh
@@ -1,23 +1,36 @@
 
 backup_function() {
-NUMBER_OF_BACKUPS=`find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | wc -l`
-if ((NUMBER_OF_BACKUPS==0)); then
-((TIME_EDIT=DATE_SEC-$4-1)) #To guarantee that a backup will be created
-else
-TIME_EDIT=`find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | sort -d --reverse| head -1 | xargs stat -c %Y`
-fi
+	if ((NUMBER_OF_BACKUPS==0)); then
+		((TIME_EDIT=DATE_SEC-$4-1)) #To guarantee that a backup will be created
+	else
+		TIME_EDIT=`find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | sort -d --reverse| head -1 | xargs stat -c %Y`
+	fi
 
 
-if ((($3)||(DATE_SEC-TIME_EDIT>=$4))); then
-cp -lr $LAST_BACKUP $NEW_BACKUP$1
-#Delete if Number of number of weekly backups is greater than MAX_WEEKLY_BACKUPS
-if ((NUMBER_OF_BACKUPS >= $2)); then
-	#Order all backups
-	find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | sort -d| head -1 | xargs rm -rf
-fi
+	if ((($3)||(DATE_SEC-TIME_EDIT>=$4))); then
+		cp -lr $LAST_BACKUP $NEW_BACKUP$1
+		#logging
+		if [ $? -eq 0 ]; then
+			echo "$(date +%H:%M) Success: Backup $1 created." >>backup_log
+		else
+			echo "$(date +%H:%M) Fail: Backup $1 could not be created." >>backup_log
+		fi
+		NUMBER_OF_BACKUPS=`find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | wc -l`
+		#Delete if Number of number of $1 backups is greater than $2
+		if ((NUMBER_OF_BACKUPS >= $2)); then
+			FILE=$(find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]"$1"$" | sort -d| head -1) #Find oldest backup
+			#logging
+			if [ $? -eq 0 ]; then
+				echo "$(date +%H:%M) Success: Number of $1 backups exceeded. $FILE deleted." >>backup_log
+			else
+				echo "$(date +%H:%M) Success: Number of $1 backups exceeded. $FILE could not be deleted." >>backup_log
+			fi
 
-fi
+		fi
+
+	fi
 }
+
 START_SEC=`date "+%s"`
 echo "Inicio: `date`"
 #Configuration
@@ -25,6 +38,7 @@ MAX_DAILY_BACKUPS=5
 MAX_WEEKLY_BACKUPS=3
 MAX_MONTHLY_BACKUPS=3
 MAX_YEARLY_BACKUPS=1
+echo "\n\n~~~~~$(date)~~~~~\n">>backup_log
 
 WEEKLY_BACKUP_DAY=6 #1 is Monday. Therefore six is Saturday.
 MONTHLY_BACKUP_DAY=20 #
@@ -49,30 +63,56 @@ echo $NEW_BACKUP
 #Read all backups to be made
 cat backup_input   | sed '/^#/ d'| sed '/^$/d' | while IFS=';' read -r NAME FOLDER_SOURCE FOLDER_DEST_OLD FOLDER_DEST_NEW OPTIONS CMD REMAINDER
 do
-#Create Dir
+#Create backup Dir
 mkdir -p "$NEW_BACKUP$FOLDER_DEST_NEW"
 
-if [[ "$CMD" == "rsync" ]]; then #Execute rsync
+####rsync case####
+if [[ "$CMD" == "rsync" ]]; then
 	eval OPTIONS=($OPTIONS)
-#Concateanate Command and Options
-cmd=(rsync -azvh --progress "${OPTIONS[@]}" --link-dest="$LAST_BACKUP$FOLDER_DEST_OLD" "$FOLDER_SOURCE" "$NEW_BACKUP$FOLDER_DEST_NEW")
+	#Concateanate Command, options and, then, execute it.
+	cmd=(rsync -azvh --progress "${OPTIONS[@]}" --link-dest="$LAST_BACKUP$FOLDER_DEST_OLD" "$FOLDER_SOURCE" "$NEW_BACKUP$FOLDER_DEST_NEW")
+	"${cmd[@]}"
 
-if [ $? -eq 0 ]; then
-echo "SUCCESS: "$(date)" ${NAME}" >>backup_log
-fi
-#Execute Command
-"${cmd[@]}"
-elif [[ "$CMD" == "cp" ]]; then #Execute Compact
-echo "SUCCESS: cp"
-NUMBER_FILES=$(find "$FOLDER_SOURCE" -mtime -1 -type f | wc -l)
-if[[ $NUMBER_FILES == 1 ]]; then
-FILE=$(find "$FOLDER_SOURCE" -mtime -1 -type f | sort -k1.1n  --reverse |head -1)
-echo "$FILE"
-time lzip -kv "$FILE"
-mv "$FILE.lz" "$NEW_BACKUP$FOLDER_DEST_NEW/$NAME.lz"
-#else
-#echo "FILE NOT FOUND - ERROR"
-$fi
+	#logging
+	if [ $? -eq 0 ]; then
+		echo "$(date +%H:%M) Success: rsync of ${NAME} was successfully finished." >>backup_log
+	else
+		echo "$(date +%H:%M) Fail: rsync of ${NAME} has failed." >>backup_log
+	fi
+
+####compact and cp case####
+elif [[ "$CMD" == "cp" ]]; then
+	NUMBER_FILES=$(find "$FOLDER_SOURCE" -mtime -1 -type f | wc -l)
+	if (( "$NUMBER_FILES" >= 1 )); then if there are recent files to be copied.
+		FILE=$(find "$FOLDER_SOURCE" -mtime -1 -type f | sort -k1.1n  --reverse |head -1)
+		echo "File to be copied: $FILE".
+		time lzip -kv "$FILE"
+
+		#logging
+		if [ $? -eq 0 ]; then
+			echo "$(date +%H:%M) Success: compress of ${NAME} was successfully finished." >>backup_log
+		else
+			echo "$(date +%H:%M) Fail: compress of ${NAME} has failed." >>backup_log
+		fi
+		mv "$FILE.lz" "$NEW_BACKUP$FOLDER_DEST_NEW/$NAME.lz"
+		#logging
+		if [ $? -eq 0 ]; then
+			echo "$(date +%H:%M) Success: ${NAME}.lz was successfully copied to backup." >>backup_log
+		else
+			echo "$(date +%H:%M) Fail: ${NAME}.lz was not copied to backup." >>backup_log
+		fi
+
+	else #if no recent file was found
+		echo "$(date +%H:%M) Warning: Daily version of ${NAME}.lz not found." >>backup_log
+		cp -l "$LAST_BACKUP$FOLDER_DEST_NEW/$NAME.lz" "$NEW_BACKUP$FOLDER_DEST_NEW/$NAME.lz"
+		#logging
+		if [ $? -eq 0 ]; then
+			echo "$(date +%H:%M) Success: A hard link of previous ${NAME}.lz was made." >>backup_log
+		else
+			echo "$(date +%H:%M) Fail: ${NAME}.lz was not copied to backup." >>backup_log
+		fi
+
+	fi
 
 fi
 done
@@ -80,7 +120,15 @@ NUMBER_OF_BACKUPS=`find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-
 
 if ((NUMBER_OF_BACKUPS > MAX_DAILY_BACKUPS)); then
 	#Order all backups
-	find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]$" | sort -d| head -1 | xargs rm -rf
+	FILE=$(find $BACKUP_FOLDER -maxdepth 1  |grep -P "BACKUP-[A-Z,a-z,0-9,:,-]*[0-9]$" | sort -d| head -1)
+	rm -rf $FILE #Remove daily bakcup
+	#logging
+	if [ $? -eq 0 ]; then
+		echo "$(date +%H:%M) Success: Number of daily backups exceeded. $FILE deleted." >>backup_log
+	else
+		echo "$(date +%H:%M) Fail: Number of daily backups exceeded, but $FILE could not be deleted." >>backup_log
+	fi
+
 fi
 
 rm -rf $LAST_BACKUP
